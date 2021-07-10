6a45e0854962107f278b0d4935bdde046f0a0c5b
ThomasN <thomasn@ucsc.edu>
2021-07-10 09:50:17 -0700

    text to identify scenes

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,7 +8,21 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-        
+        //text format
+        let gameText = {
+            fontFamily: 'Impact',
+            fontSize: '28px',
+            color: '#000000',
+            backgroundColor: '#F3B141',
+            align: 'center',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
+        this.add.text(game.config.width/2, game.config.height/2, 'Play Scene', gameText).setOrigin(0.5);
     }
 
     update() {
