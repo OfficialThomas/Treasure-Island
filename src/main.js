8e5d102848ac8bb9cb75cd8b08a06156fe2f5705
ThomasN <thomasn@ucsc.edu>
2021-07-11 19:51:29 -0700

    added timer variable for end screen

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [ Menu, Play, End ]
+    scene: [ Menu, Play, End]
 }
 
 let game = new Phaser.Game(config);
 
 //reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
+//global stat vars
+let timeScore = 0;
 
 //set UI sizes
 let borderUISize = game.config.height / 15;
