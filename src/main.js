7ffc7f689e95f9646b9085b2869fc218d296e722
ThomasN <thomasn@ucsc.edu>
2021-07-11 19:48:24 -0700

    Added End Scene

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [ Menu, Play ]
+    scene: [ Menu, Play, End ]
 }
 
 let game = new Phaser.Game(config);
 
 //reserve keyboard vars
-let keyF, keyR, keyLEFT, keyRIGHT;
 
 //set UI sizes
 let borderUISize = game.config.height / 15;
---
+++ src/scenes/End.js
@@ -0,0 +1,31 @@
+class End extends Phaser.Scene {
+    constructor() {
+        super("endScene");
+    }
+
+    preload(){
+
+    }
+
+    create() {
+        //text format
+        let gameText = {
+            fontFamily: 'Impact',
+            fontSize: '28px',
+            color: '#000000',
+            backgroundColor: '#F3B141',
+            align: 'center',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
+        this.add.text(game.config.width/2, game.config.height/2, 'End Scene', gameText).setOrigin(0.5);
+    }
+
+    update(){
+        this.scene.start('playScene');
+    }
+}
