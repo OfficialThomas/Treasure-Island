25fd89f66be340b90d3dd2c2634ae79ce5e09db2
tnoell <tnoell@ucsc.edu>
2021-07-21 17:48:31 -0700

    drum character

--- src/scenes/End.js
+++ src/scenes/End.js
@@ -7,6 +7,8 @@ class End extends Phaser.Scene {
         //background
         this.load.image('endScore', './assets/EndScreen-OrangeFilter.png');
 
+        //drum chan
+        this.load.spritesheet('drum', './assets/enemy_newRed-Sheet.png', { frameWidth: 48, frameHeight: 32 });
     }
 
     create() {
@@ -30,6 +32,15 @@ class End extends Phaser.Scene {
         //background
         this.add.image(0, 0, 'endScore').setOrigin(0, 0);
         this.add.text(game.config.width/2 + borderUISize*4, game.config.height - borderUISize*1.25, chestCount, gameText).setOrigin(0.5);
+        
+        //drum chan <3
+        const drumAnimation = this.anims.create({ 
+            key: 'drumWalk',
+            frames: this.anims.generateFrameNumbers('drum'),
+            frameRate: 8
+        });
+        this.drumChan = this.add.sprite(game.config.width/2, game.config.height/2 + borderUISize*2, 'drum');
+        this.drumChan.play({key: 'drumWalk', repeat: -1});
     }
 
     update(){
